f496ae94941a5a240bb0c29dae8f3e8c8f7378ca
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-05-28 12:15:15 +0300

    Ex_4

---
+++ Aggregation_and_composition/Ex_4/Account.java
@@ -0,0 +1,46 @@
+package Lesson_04_Aggregation_composition.Ex_4;
+
+class Account {
+
+    private static long startId = 0;
+    private long id;
+    private double balance;
+    private boolean isOpen = true;
+
+    public Account(double balance) {
+        this.balance = balance;
+        this.id = ++startId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public String getStatus() {
+        String status = isOpen ? "Unlocked" : "Blocked";
+        return String.format("Account %d %s.", id, status);
+    }
+
+    public void block(){
+        isOpen = false;
+    }
+
+    public void unlock() {
+        isOpen = true;
+    }
+
+
+}
+
